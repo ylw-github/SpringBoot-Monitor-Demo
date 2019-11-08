3262b62960c20c02d3dc1cbd5ce7b5edb695987a
yanglinwei <1962264482@qq.com>
2019-11-08 11:30:57 +0800

    Actuator监控应用

---
+++ src/main/java/com/ylw/springboot/App.java
@@ -0,0 +1,14 @@
+package com.ylw.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+
+@EnableAutoConfiguration
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
